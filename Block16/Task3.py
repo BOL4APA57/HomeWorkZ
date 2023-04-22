99450caf7e2b133e02009c348efd7389fee0b8d1
BOL4APA57 <flock.werewolf@gmail.com>
2023-04-22 12:55:42 +0300

    Task3 переделал. :)

--- Block16/Task3.py
+++ Block16/Task3.py
@@ -27,15 +27,8 @@
 
 import random
 
-# def start
-def list(count):
-    ls = []
-    for _ in range(count):
-        ls.append(round(random.uniform(5, 10), 2))
-    return ls
+first = [round(random.uniform(5, 10), 2) for _ in range(20)]
+second = [round(random.uniform(5, 10), 2) for i in range(20)]
 
-# def end
-
-first, second = list(20), list(20)
 winners = [first[i] if first[i] >= second[i] else second[i] for i in range(20)]
 print(f"Первая команда: {first}\nВторая команда: {second}\n \nПобедители тура: {winners}")
